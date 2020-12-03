c86d59493f2aadf037179b241b7457accb8895ec
Siphra <47285212+Siphra@users.noreply.github.com>
2020-12-03 17:08:45 -0500

    commit

--- AB testing file.py
+++ AB testing file.py
@@ -1,13 +1,33 @@
 # AB Testing file
 # Initial menu code laid down 3 December 2020
 # Project actual start date:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
-def menu_ops:
+def menu_ops():
+
+    """
+    This function serves as the main menu for all other function calls,
+    it's purpose is to reduce the amount of processing time to the bare minimum
+    by not forcing the code to call all available functions inorder to test, or run
+    the code.
+
+    """
+
     print(" This menu is to make the code easier to use")
     print(" Selection menu:")
-    input(print(" What option would you like "), x)
-    print(x)
+    menu = ['1: First option', '2: Second Option']
+    for i in range(len(menu)):
+        print(menu[i])
+    print("What option would you like : ")
+    x = input()
+    if x == int:
+        print(menu(x))
+    else:
+        print(x)
+    return print(x)
 
-menu_ops()
+help(menu_ops)
