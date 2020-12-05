b38325d43bab3ed86a924ca88a184ca2c170475e
Siphra <47285212+Siphra@users.noreply.github.com>
2020-12-05 10:45:19 -0500

    updates to statsmodel prac

--- statsmodels practice.py
+++ statsmodels practice.py
@@ -2,10 +2,17 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+# lesson 15 multiple linear regression question 4 has a mistake in its grading
 
-df = pd.read_csv(r'I:\Python\PycharmProjects\udacityproject1\csv from classes\house_price_area_only.csv')
+df = pd.read_csv(r'I:\Python\PycharmProjects\udacityproject1\csv from classes\house_prices.csv')
 df['intercept'] = 1
+dfs = pd.get_dummies(df['style'])
+dfn = pd.get_dummies(df['neighborhood'])
 
-lm = sm.OLS(df.price, df[['intercept', 'area']])
+df_new = df.join(dfn)
+df_new = df_new.join(dfs)
+
+
+lm = sm.OLS(df_new.price, df_new[['intercept','bathrooms','bedrooms','lodge','victorian','B','C']])            #simple linear model
 results = lm.fit()
 print(results.summary())
