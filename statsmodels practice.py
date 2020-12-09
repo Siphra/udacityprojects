1db02dfb7006c876f7f8aaab0e21c390a9dc396b
Siphra <47285212+Siphra@users.noreply.github.com>
2020-12-08 20:13:54 -0500

    SQL learning
    buch of sql crud to break the boredom... but its all SELECT basic FROM your_memory

--- statsmodels practice.py
+++ statsmodels practice.py
@@ -49,10 +49,10 @@ lm = sm.Logit(df['fraud'],df[['intercept','weekday','duration']])
 results = lm.fit()
 
 df = pd.read_csv(r'I:\Python\PycharmProjects\udacityproject1\csv from classes\admissions.csv')
-df[['1','2','3','4']] = pd.get_dummies(df.prestige)
+df[['1', '2', '3', '4']] = pd.get_dummies(df.prestige)
 df['intercept'] = 1
-df.drop(['1'], axis=1, inplace=True)
-log_mod = sm.Logit(df.admit, df[['intercept','gre','gpa','2','3','4']])
+#df.drop(['1'], axis=1, inplace=True)
+log_mod = sm.Logit(df.admit, df[['intercept', 'gre', 'gpa', '2', '3', '4']])
 results = log_mod.fit()
 gre_eb = math.exp(.0022)
 gpa_eb = math.exp(.7793)
@@ -60,5 +60,50 @@ pre_eb = math.exp(-1.3387)
 gpa_ebi = 1/gpa_eb
 pre_ebi = 1/pre_eb
 
-y= df['admit']
+y = df['admit']
+X = df[['gre', 'gpa', '1', '2', '3']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+log_mod = LogisticRegression()
+log_mod.fit(X_train, y_train)
+y_pred = log_mod.predict(X_test)
+print(df.head())
+print(precision_score(y_test, y_pred))
+print(recall_score(y_test, y_pred))
+print(accuracy_score(y_test ,y_pred))
+print(confusion_matrix(y_test, y_pred))
 
+#below is project code
+
+df = pd.read_csv(r'I:\Python\PycharmProjects\udacityproject1\csv from classes\ab_data.csv')
+print(df.head())
+print(df.info())
+print(df.nunique())
+print(sum(df.converted)/df.user_id.nunique())
+dfchk = df[df['group'] == 'treatment']
+dfchkop = df[df['group'] == 'control']
+dfchk = dfchk[dfchk['landing_page']== 'old_page']
+dfchkop = dfchkop[dfchkop['landing_page'] == 'new_page']
+dfchk = dfchk.append(dfchkop)
+print(dfchk.info())
+df2 = df[~df.isin(dfchk)].dropna()
+print(df2.head())
+print(df2[((df2['group'] == 'treatment') == (df2['landing_page'] == 'new_page')) == False].shape[0])
+print(df2.nunique())
+dups = df2[df2.user_id.duplicated(keep = False)==True]
+print(dups)
+df2.drop(2893, axis=0, inplace=True)
+print(df2.shape)
+conv = sum(df2.converted)/df2.shape[0]
+print(conv)
+dfc = df2[df2.group == 'control']
+print(sum(dfc.converted)/dfc.shape[0])
+dft = df2[df2.group == 'treatment']
+print(sum(dft.converted)/dft.shape[0])
+print(sum(df2.group == 'treatment')/df2.shape[0])
+print(conv,conv, dft.shape[0], dfc.shape[0])
+
+new_page_converted = np.random.choice([0,1], dft.shape[0],replace= True, p=[conv,1-conv])
+old_page_converted = np.random.choice([0,1], dfc.shape[0], replace= True, p=[conv, 1-conv])
+plt.hist(new_page_converted, color='green')
+plt.hist(old_page_converted, color='red')
+plt.show()
